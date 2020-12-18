91eede052ae363142992dcd8e919416d5221f80a
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-12-18 20:41:30 -0300

    feat: add basic example of sent a message

--- index.php
+++ index.php
@@ -7,4 +7,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-var_dump((new Notify())->getAllEnvironments());
+$messenger = new Notify();
+
+$messenger->sendAlertMessage('Olá mundo');
--- source/App/Notify.php
+++ source/App/Notify.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace Source\App;
 
+use TelegramBot\Api\BotApi;
+
 class Notify
 {
-    public function getAllEnvironments()
+    /**
+     * Send an alert message
+     *
+     * @param string $message
+     * @return bool
+     */
+    public function sendAlertMessage(string $message): bool
     {
-        return $_ENV['CONF_TELEGRAM_BOT_TOKEN'];
+        $botApi = new BotApi($_ENV['CONF_TELEGRAM_BOT_TOKEN']);
+        
+        try {
+            $botApi->sendMessage($_ENV['CONF_TELEGRAM_CHAT_ID'], $message);
+
+            return true;
+        } catch (\Exception $exception) {
+            return false;
+        }
     }
 }
